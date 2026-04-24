e7b235d645075426a1c618d7cc644b9966518465
bslater <ben@theslaters.com.au>
2026-04-25 09:23:32 +1000

    Create TwoFishTransformTrsts.cs

---
+++ Bodu.Security.Cryptography/test/Security.Cryptography/TwoFishTransformTrsts.cs
@@ -0,0 +1,25 @@
+/ ---------------------------------------------------------------------------------------------------------------
+// <copyright file="BlowfishTransformTests.cs" company="PlaceholderCompany">
+//     Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------
+
+namespace Bodu.Security.Cryptography;
+
+/// <summary>
+/// Concrete test class that exercises the <see cref="BlockCipherTransformTests{TCryptoTransform}" /> base tests
+/// against the <see cref="TwoFishTransform" /> implementation.
+/// </summary>
+[TestClass]
+internal sealed class TwoFishTransformTests
+    : BlockCipherTransformTests<TwoFishTransform>
+{
+    /// <inheritdoc />
+    protected override TwoFishTransform CreateAlgorithm()
+    {
+        var algorithm = new TwoFish();
+        algorithm.GenerateKey();
+        algorithm.GenerateIV();
+        return (TwoFishTransform)algorithm.CreateEncryptor(algorithm.Key, algorithm.IV);
+    }
+}
