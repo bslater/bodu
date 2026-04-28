a6098f645a3a68645e660e0f46e98de001b83690
bslater <ben@theslaters.com.au>
2026-04-28 13:46:42 +1000

    feat(IO.Hashing): add NonCryptographicHashAlgorithmExtensions with full test coverage (#120)

---
+++ Bodu.IO.Hashing/src/IO.Hashing.Extensions/NonCryptographicHashAlgorithmExtensions.cs
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------
+// <copyright file="NonCryptographicHashAlgorithmExtensions.cs" company="PlaceholderCompany">
+//     Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------
+
+using System.IO.Hashing;
+
+namespace Bodu.IO.Hashing.Extensions;
+
+/// <summary>
+/// Provides a set of <see langword="static" /> (<see langword="Shared" /> in Visual Basic) extension methods that extend the
+/// <see cref="NonCryptographicHashAlgorithm" /> class with verification, comparison, and incremental hashing capabilities.
+/// </summary>
+public static partial class NonCryptographicHashAlgorithmExtensions
+{
+}
